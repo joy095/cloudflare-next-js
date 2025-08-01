058996ebb921569ad3a39ae7cf8fd191e3bd9353
Joy <joykarmakar987654321@gmail.com>
2025-08-01 21:05:39 +0530

    image formate defined

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,9 @@
 import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
-
-// Here we use the @cloudflare/next-on-pages next-dev module to allow us to
-// use bindings during local development (when running the application with
-// `next dev`). This function is only necessary during development and
-// has no impact outside of that. For more information see:
-// https://github.com/cloudflare/next-on-pages/blob/main/internal-packages/next-dev/README.md
 setupDevPlatform().catch(console.error);
 
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
   images: {
     remotePatterns: [
@@ -27,6 +20,7 @@ const nextConfig: NextConfig = {
         hostname: 'picsum.photos',
       },
     ],
+    formats: ['image/avif', 'image/webp'], // Only AVIF and WebP
   },
 };
 
